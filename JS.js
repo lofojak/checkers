84e643e0afcc88d51430439ab69658c15fd91128
lofojak <lofojak@gmail.com>
2022-08-21 00:06:45 +0300

    Переделка. Уменьшил нагрузку на JS, увеличил на CSS

--- JS.js
+++ JS.js
@@ -5,6 +5,10 @@ var Wcoord = new Array(1, 3, 5, 7, 10, 12, 14, 16, 17, 19, 21, 23);
 var Bcoord = new Array(42, 44, 46, 48, 49, 51, 53, 55, 58, 60, 62, 64);
 var step = -1;
 var pervpoint;
+/*const WPeshkainHTML = "<div class='WPeshka'><div>";
+const BPeshkainHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #644;'><div>";
+const 
+*/
 
 function init() {
 	document.getElementBy
@@ -38,10 +42,10 @@ function Start() {
 			B[i].style.left = (12 + 79 * ((Bcoord[i] - 1) % 8)) + "px";
 		}
 		for (var i = 0; i < 12; i++) {
-			C[Wcoord[i]].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #fff; position: inherit; margin: 1px; background: #bcc;'><div>";
+			C[Wcoord[i]].innerHTML = "<div class='PeshkaW'><div>";
 		}
 		for (var i = 0; i < 12; i++) {
-			C[Bcoord[i]].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #644;'><div>";
+			C[Bcoord[i]].innerHTML = "<div class='PeshkaB'><div>";
 		}
 	}
 }
@@ -50,44 +54,45 @@ function Click(id) {
 	if (isOnBoard(id)) {
 		if (step >= 0) {
 			if (step % 4 == 0) {
+				//Этап выбора перемещения белых
 				if (Wcoord.indexOf(id) === -1) {
 					alert('Сейчас играют белые');
 				}
 				else {
 					pervpoint = id;
-					C[id].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #999;'><div>";
+					C[id].innerHTML = "<div class='PeshkaW PeshkaWPoint'><div>";
 					step++;
 				}
 			}
 			else if (step % 2 == 1) {
 				if (Wcoord.indexOf(pervpoint) >= 0) {
-					if (Wcoord.indexOf(id) >= 0) {
-						C[pervpoint].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #fff; position: inherit; margin: 1px; background: #bcc;'><div>";
+					if (Wcoord.indexOf(id) >= 0) { // Случай перевыбора
+						C[pervpoint].innerHTML = "<div class='PeshkaW'><div>";
 						pervpoint = id;
-						C[id].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #999;'><div>";
+						C[id].innerHTML = "<div Class='PeshkaW PeshkaWPoint'><div>";
 					}
-					else if (Bcoord.indexOf(id) >= 0) {
+					else if (Bcoord.indexOf(id) >= 0) { // Случай сруба вражеской пешки
 						alert('срубил');
 					}
-					else {
+					else { //Случай хода
 						C[pervpoint].innerHTML = "";
-						C[id].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #bcc;'><div>";
+						C[id].innerHTML = "<div class='PeshkaW'><div>";;
 						Wcoord.splice(Wcoord.indexOf(pervpoint), 1, id);
 						step++;
 					}
 				} 
 				else if (Bcoord.indexOf(pervpoint) >= 0) {
-					if (Wcoord.indexOf(id) >= 0) {
+					if (Wcoord.indexOf(id) >= 0) { //Случай сруба 
 						alert('срубил');
 					}
-					else if (Bcoord.indexOf(id) >= 0) {
-						C[pervpoint].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #644;'><div>";
+					else if (Bcoord.indexOf(id) >= 0) { //Случай перевыбора
+						C[pervpoint].innerHTML = "<div Class='PeshkaB'><div>";
 						pervpoint = id;
-						C[id].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #a88;'><div>";
+						C[id].innerHTML = "<div Class='PeshkaB PeshkaBPoint'><div>";
 					}
-					else {
+					else { // случай хода 
 						C[pervpoint].innerHTML = "";
-						C[id].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #644;'><div>";
+						C[id].innerHTML = "<div Class='PeshkaB'><div>";
 						Bcoord.splice(Bcoord.indexOf(pervpoint), 1, id);
 						step++;
 					}
@@ -100,7 +105,7 @@ function Click(id) {
 				}
 				else {
 					pervpoint = id;
-					C[id].innerHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #a88;'><div>";
+					C[id].innerHTML = "<div Class='PeshkaB PeshkaBPoint'><div>";
 					step++;
 				}
 			}
