9a82d1151f9af93e80871bb2751df1fcda766bc5
lofojak <lofojak@gmail.com>
2022-08-21 00:32:55 +0300

    Рефракторинг кода на JS

--- JS.js
+++ JS.js
@@ -5,10 +5,6 @@ var Wcoord = new Array(1, 3, 5, 7, 10, 12, 14, 16, 17, 19, 21, 23);
 var Bcoord = new Array(42, 44, 46, 48, 49, 51, 53, 55, 58, 60, 62, 64);
 var step = -1;
 var pervpoint;
-/*const WPeshkainHTML = "<div class='WPeshka'><div>";
-const BPeshkainHTML = "<div style='border-radius: 50%; width: 66px; height: 66px; border: 1px solid #888; position: inherit; margin: 1px; background: #644;'><div>";
-const 
-*/
 
 function init() {
 	document.getElementBy
@@ -52,66 +48,9 @@ function Start() {
 
 function Click(id) {
 	if (isOnBoard(id)) {
-		if (step >= 0) {/*
-			if (step % 4 == 0) {
-				//Этап выбора перемещения белых
-				if (Wcoord.indexOf(id) === -1) {
-					alert('Сейчас играют белые');
-				}
-				else {
-					pervpoint = id;
-					C[id].innerHTML = "<div class='PeshkaW PeshkaWPoint'><div>";
-					step++;
-				}
-			}
-			else if (step % 2 == 1) {
-				if (Wcoord.indexOf(pervpoint) >= 0) {
-					if (Wcoord.indexOf(id) >= 0) { // Случай перевыбора
-						C[pervpoint].innerHTML = "<div class='PeshkaW'><div>";
-						pervpoint = id;
-						C[id].innerHTML = "<div Class='PeshkaW PeshkaWPoint'><div>";
-					}
-					else if (Bcoord.indexOf(id) >= 0) { // Случай сруба вражеской пешки
-						alert('срубил');
-					}
-					else { //Случай хода
-						C[pervpoint].innerHTML = "";
-						C[id].innerHTML = "<div class='PeshkaW'><div>";;
-						Wcoord.splice(Wcoord.indexOf(pervpoint), 1, id);
-						step++;
-					}
-				} 
-				else if (Bcoord.indexOf(pervpoint) >= 0) {
-					if (Wcoord.indexOf(id) >= 0) { //Случай сруба 
-						alert('срубил');
-					}
-					else if (Bcoord.indexOf(id) >= 0) { //Случай перевыбора
-						C[pervpoint].innerHTML = "<div Class='PeshkaB'><div>";
-						pervpoint = id;
-						C[id].innerHTML = "<div Class='PeshkaB PeshkaBPoint'><div>";
-					}
-					else { // случай хода 
-						C[pervpoint].innerHTML = "";
-						C[id].innerHTML = "<div Class='PeshkaB'><div>";
-						Bcoord.splice(Bcoord.indexOf(pervpoint), 1, id);
-						step++;
-					}
-				}
-				else alert('Error');
-			}
-			else {
-				if (Bcoord.indexOf(id) === -1) {
- 					alert('Сейчас играют чёрные');
-				}
-				else {
-					pervpoint = id;
-					C[id].innerHTML = "<div Class='PeshkaB PeshkaBPoint'><div>";
-					step++;
-				}
-			} */
-			if (step % 2 == 0) { //Ход белых
-				if (isNaN(pervpoint)) {
-					//Этап выбора перемещения белых
+		if (step >= 0) {
+			if (step % 2 == 0) { 				// Ход белых
+				if (isNaN(pervpoint)) {					// Этап выбора шашки, которая будет делать ход
 					if (Wcoord.indexOf(id) === -1) {
 						alert('Сейчас играют белые');
 					}
@@ -120,16 +59,16 @@ function Click(id) {
 						C[id].innerHTML = "<div class='PeshkaW PeshkaWPoint'><div>";
 					}
 				}
-				else {
-					if (Wcoord.indexOf(id) >= 0) { // Случай перевыбора
+				else {									// Этап выбора пути, куда будет делать ход шашка
+					if (Wcoord.indexOf(id) >= 0) {				// Случай перевыбора
 						C[pervpoint].innerHTML = "<div class='PeshkaW'><div>";
 						pervpoint = id;
 						C[id].innerHTML = "<div Class='PeshkaW PeshkaWPoint'><div>";
 					}
-					else if (Bcoord.indexOf(id) >= 0) { // Случай сруба вражеской пешки
+					else if (Bcoord.indexOf(id) >= 0) {			// Случай сруба вражеской пешки
 						alert('срубил');
 					}
-					else { //Случай хода
+					else {										// Случай хода
 						C[pervpoint].innerHTML = "";
 						C[id].innerHTML = "<div class='PeshkaW'><div>";;
 						Wcoord.splice(Wcoord.indexOf(pervpoint), 1, id);
@@ -138,8 +77,8 @@ function Click(id) {
 					}
 				}
 			}
-			else { // Ход чёрных
-				if (isNaN(pervpoint)) {
+			else {								// Ход чёрных
+				if (isNaN(pervpoint)) {					// Этап выбора шашки, которая будет делать ход
 					if (Bcoord.indexOf(id) === -1) {
 	 					alert('Сейчас играют чёрные');
 					}
@@ -148,16 +87,16 @@ function Click(id) {
 						C[id].innerHTML = "<div Class='PeshkaB PeshkaBPoint'><div>";
 					}
 				}
-				else {
-					if (Wcoord.indexOf(id) >= 0) { //Случай сруба 
+				else {									// Этап выбора пути хода шашки
+					if (Wcoord.indexOf(id) >= 0) {				// Случай сруба 
 						alert('срубил');
 					}
-					else if (Bcoord.indexOf(id) >= 0) { //Случай перевыбора
+					else if (Bcoord.indexOf(id) >= 0) {			// Случай перевыбора
 						C[pervpoint].innerHTML = "<div Class='PeshkaB'><div>";
 						pervpoint = id;
 						C[id].innerHTML = "<div Class='PeshkaB PeshkaBPoint'><div>";
 					}
-					else { // случай хода 
+					else {										// Случай хода 
 						C[pervpoint].innerHTML = "";
 						C[id].innerHTML = "<div Class='PeshkaB'><div>";
 						Bcoord.splice(Bcoord.indexOf(pervpoint), 1, id);
